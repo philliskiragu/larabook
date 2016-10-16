1507ab933b656d3e818594b1a687c1c94c158a87
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-10-16 17:27:25 +0300

    Returning the tests to green

--- tests/functional/SignInCept.php
+++ tests/functional/SignInCept.php
@@ -5,7 +5,6 @@ $I->wantTo('login to my larabook account');
 
 $I->signIn();
 $I->seeInCurrentUrl('/statuses');
-$I->see('Post a status');
-//$I->assertTrue(Auth::check());
+$I->assertTrue(Auth::check());
 
 
