92baa33c6e1a8cd2447ddda9546cbbcc0a77944c
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-10-18 17:26:27 +0300

    Add profile button to the welcomepage

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -4,16 +4,21 @@
 
     <div class="jumbotron">
         <h1>Welcome to Larabook</h1>
+
         <p>Welcome to the premier place to talk about Laravel with others.</p>
 
-        @if (! $currentUser)
+        @if ($currentUser)
+            <p>Do you want to post a status? Go to your Profile</p>
+            <p>
+                {{ link_to_route('profile_path','Profile', $currentUser->username, ['class' => 'btn btn-lg btn-primary'])}}
+            </p>
+        @else
             <p>Sign up to see what its all about!</p>
-        <p>
-            {{ link_to_route('register_path','Sign Up!', null, ['class' => 'btn btn-lg btn-primary'])}}
-        </p>
-            @endif
-
+            <p>
+                {{ link_to_route('register_path','Sign Up!', null, ['class' => 'btn btn-lg btn-primary'])}}
+            </p>
+        @endif
     </div>
 
 
-    @stop
+@stop
