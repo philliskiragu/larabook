ebb1ae6d43f85d246e53013c0fbb5267a88253b2
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-10-19 13:23:33 +0300

    Add custom 404 error page

---
+++ app/views/errors/403.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+          crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Roboto|Josefin+Sans|Work+Sans' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <title>Larabook</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="span12">
+            <div class="hero-unit center">
+                <h1>No Access
+                    <small><font face="Work San" color="red">Error 403</font></small>
+                </h1>
+                <br/>
+
+                <p>You do not have access to this page. Use your
+                    browsers <b>Back</b> button to navigate to the page you came from</p>
+
+                <p><b>Or you could just got to the homepage</b></p>
+                <a href="/" class="btn btn-large btn-info"><i class="icon-home icon-white"></i> Take Me Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
---
+++ app/views/errors/404.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+          crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Roboto|Josefin+Sans|Work+Sans' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <title>Larabook</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="span12">
+            <div class="hero-unit center">
+                <h1>Page Not Found
+                    <small><font face="Work San" color="#00BAE9">Error 404</font></small>
+                </h1>
+                <br/>
+
+                <p>The page you requested could not be found, check the url and try again. Use your
+                    browsers <b>Back</b> button to navigate to the page you came from</p>
+
+                <p><b>Or you could just got to the homepage</b></p>
+                <a href="/" class="btn btn-large btn-info">Take Me Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
